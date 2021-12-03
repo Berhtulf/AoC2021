710e1ec3a7792fb0323bfab91888d6f565cd7d1a
Václavík Martin <mvaclavik96@icloud.com>
2021-12-03 09:01:21 +0100

    Initial Commit

---
+++ AoC2021/AoC2021App.swift
@@ -0,0 +1,17 @@
+//
+//  AoC2021App.swift
+//  AoC2021
+//
+//  Created by Martin Václavík on 03.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct AoC2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
