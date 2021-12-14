3e8fdec4acaf079cdbd7c88d2d5931f307716e50
Václavík Martin <mvaclavik96@icloud.com>
2021-12-14 20:20:46 +0100

    initial commit

--- AoC2021/ContentView.swift
+++ AoC2021/ContentView.swift
@@ -6,20 +6,55 @@
 //
 
 import SwiftUI
+import os
 
 struct ContentView: View {
     @ObservedObject var viewModel = ViewModel()
+    @State var day: Int = 1
+    @State var importingFile = false
+    
+    @State var data = ""
     
     @State var showResult = false
     var body: some View {
         NavigationView{
-            ScrollView{
-                ForEach(1...24, id: \.self, content: { i in
-                    NavigationLink("Day \(i)") {
-                        ResultsView(viewModel: viewModel, day: i)
-                    }.disabled(!viewModel.implementedDays.contains(i))
-                })
-            }
+            VStack(alignment: .leading){
+                Section{
+                    Picker("Solve day", selection: $day) {
+                        ForEach(viewModel.implementedDays, id: \.self){ dayIndex in
+                            Text("\(dayIndex)").tag(dayIndex)
+                        }
+                    }
+                } header: {
+                    Text("Configuration")
+                }
+                Divider()
+                Section{
+                    Button("Import file") {
+                        importingFile = true
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .fileImporter(isPresented: $importingFile,
+                                  allowedContentTypes: [.plainText],
+                                  allowsMultipleSelection: false,
+                                  onCompletion: { result in
+                        do {
+                            guard let selectedFile: URL = try result.get().first else { return }
+                            guard let inputString = String(data: try Data(contentsOf: selectedFile), encoding: .utf8) else { return }
+                            
+                            data = inputString
+                        } catch {
+                            let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "fileimport")
+                            logger.log("Failed to load file data")
+                        }
+                    })
+                    TextField("Or enter input", text: $data, prompt: nil)
+                } header : {
+                    Text("Source data")
+                }
+
+                Spacer()
+            }.padding()
         }
     }
 }
